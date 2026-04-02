4b3443bd9b6d11689376559ef3a7d3f36a3ba3e8
Huy Tang <htang.prof@outlook.com>
2026-04-01 19:13:02 -0700

    fix: resolve merge conflict with main

--- authentication/views.py
+++ authentication/views.py
@@ -2,14 +2,24 @@ from rest_framework import status
 from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken, TokenError
 
-from .serializers import (
+from authentication.serializers import (
     SignUpSerializer,
     LoginSerializer,
     LogoutSerializer,
+    ResendVerificationSerializer
 )
-from .utils import update_user_login_metadata
+from authentication.services.mailing import (
+    handle_email_verification,
+    verify_token,
+)
+from authentication.services.user import (
+    get_user_by_refresh_token,
+    update_user_login_metadata,
+    update_user_logout_metadata,
+)
 
 
 class SignUpEndpoint(CreateAPIView):
@@ -50,8 +60,54 @@ class LogoutEndpoint(GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        refresh = serializer.validated_data["refresh"]
+        refresh_token = request.data.get("refresh")
+
+        try:
+            # Retrieve the owner
+            user = get_user_by_refresh_token(refresh_token)
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except TokenError:
+            return Response({
+                "error": "Token is invalid or already blacklisted."
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        updated_user = update_user_logout_metadata(user, request)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class VerifyEmailEndpoint(APIView):
+    """Endpoint for verifying user email"""
+    permission_classes = [AllowAny]
+
+    def get(self, request):
+        token = request.query_params.get("token")
+        if not token:
+            return Response({
+                "error": "Token is required."
+            },status=status.HTTP_400_BAD_REQUEST)
+
+        verify_token(token)
+
+        return Response({
+            "message": "Email verified successfully."
+        }, status=status.HTTP_200_OK)
+
+
+class ResendVerificationEndpoint(GenericAPIView):
+    """Endpoint for resending verification email"""
+    serializer_class = ResendVerificationSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+
+        handle_email_verification(user)
+
+        return Response({
+            "message": f"A verification email has been sent to {user.email}."
+        }, status=status.HTTP_200_OK)
+
